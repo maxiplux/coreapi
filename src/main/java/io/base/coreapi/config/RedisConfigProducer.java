6cc276de491f7c945261bd6d40fc39e6d1b431df
Juan Francisco Mosquera <maxiplux@gmail.com>
2022-06-13 04:57:07 -0400

    Deploy railway

--- src/main/java/io/base/coreapi/config/RedisConfigProducer.java
+++ src/main/java/io/base/coreapi/config/RedisConfigProducer.java
@@ -3,6 +3,7 @@ package io.base.coreapi.config;
 
 import io.base.coreapi.model.Ocupacion;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -13,10 +14,31 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @Slf4j
 public class RedisConfigProducer {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private Integer redisPort;
+    @Value("${spring.redis.username}")
+    private String redisUsername;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Bean
     JedisConnectionFactory connectionFactory() {
+        log.info("Status Redis H:{} - P:{} - U:{} - PW:{}", this.redisHost, this.redisPort, this.redisUsername, this.redisPassword);
+
         JedisConnectionFactory factory = new JedisConnectionFactory();
         // todo: move to app properties
+        factory.setPort(this.redisPort);
+        factory.setHostName(this.redisHost);
+        if (!this.redisPassword.isEmpty())
+        {
+            factory.setPassword(this.redisPassword);
+
+        }
+        factory.setUsePool(true);
         factory.getPoolConfig().setMaxIdle(30);
         factory.getPoolConfig().setMinIdle(30);
         return factory;
